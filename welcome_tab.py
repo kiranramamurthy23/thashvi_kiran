d65c375f56c0f7e74ee74c60fa7290976dd08c1f
Kiran Ramamurthy <117091184+kiranramamurthy23@users.noreply.github.com>
2023-03-26 12:57:05 +0530

    Update welcome_tab.py

--- welcome_tab.py
+++ welcome_tab.py
@@ -1,7 +1,7 @@
 import streamlit as st
 def welcome_tab():
 
-    display_text = "Please choose the use case from the list present in the sidebar menu"
+    display_text = "Welcome to the world of my little princess. Click the menu from side bar"
 
     st.markdown("<body style='font-size:40px;text-align:center;font-weight:bold;'>" + display_text + "</body>",
                 unsafe_allow_html=True)
