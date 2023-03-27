226ec3464c9c5ac700bd1d9c2036d86136dff43b
Kiran Ramamurthy <117091184+kiranramamurthy23@users.noreply.github.com>
2023-03-27 05:14:19 +0000

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -70,25 +70,3 @@ def add_logo_img(image_path):
             image_path=image_path
         )
     st.markdown(logo_image, unsafe_allow_html=True)
-    
-
-    
-@st.cache_data
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
