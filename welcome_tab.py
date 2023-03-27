d7613621bcb74773b83bf87bd4d23039f00a5844
Kiran Ramamurthy <117091184+kiranramamurthy23@users.noreply.github.com>
2023-03-27 05:02:18 +0000

    Delete welcome_tab.py

--- welcome_tab.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-def welcome_tab():
-
-    display_text = "Welcome to the world of my little princess. Click the menu from side bar"
-
-    st.markdown("<body style='font-size:40px;text-align:center;font-weight:bold;'>" + display_text + "</body>",
-                unsafe_allow_html=True)
