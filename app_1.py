6b69fcafa1eb233b1133fe9f840bec0cb1ea5889
Kiran Ramamurthy <117091184+kiranramamurthy23@users.noreply.github.com>
2023-03-27 06:15:48 +0000

    Update app_1.py

--- app_1.py
+++ app_1.py
@@ -25,7 +25,7 @@ font-size: 24px;font-weight: 600;}</style>""", unsafe_allow_html=True)
 ################### USAA LOGO ###########################
 
 st.markdown(f"""<div class='logo-begining'></div>""", unsafe_allow_html=True)
-# add_logo_img("logo1.png")
+add_logo_img("logo1.png")
 # add_logo_gif("Thashu_V1.gif")
 with open("style_test.css") as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
