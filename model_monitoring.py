a3d00224e445185c3bde865bba9088d6d4aa8069
Kiran Ramamurthy <117091184+kiranramamurthy23@users.noreply.github.com>
2023-03-27 05:01:36 +0000

    Delete model_monitoring.py

--- model_monitoring.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def model_monitoring(usecase):
-    if usecase == 'Mortgage Loan':
-        display_text = "After model deployment, the reports are displayed below"
-    
-        st.markdown("<body style='font-size:40px;text-align:center;font-weight:bold;'>"+display_text+"</body>", unsafe_allow_html=True)
