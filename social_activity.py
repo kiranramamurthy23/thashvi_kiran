53c9e651f1afc8de5313049642449cfd72a46a3a
Kiran Ramamurthy <117091184+kiranramamurthy23@users.noreply.github.com>
2023-04-09 17:04:52 +0000

    Delete social_activity.py

--- social_activity.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def social_activity():
-    # st.markdown("#### Availability of data:")
-    st.radio("Availability of data:",['Yes',"No"],label_visibility="visible",index=0)
-    
-    st.write("---------")
