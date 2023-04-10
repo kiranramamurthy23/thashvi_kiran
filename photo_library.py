89890737d62c43e8533b094a538cc8fa9d551c26
Kiran Ramamurthy <117091184+kiranramamurthy23@users.noreply.github.com>
2023-04-10 16:54:07 +0530

    Update photo_library.py

--- photo_library.py
+++ photo_library.py
@@ -1,10 +1,18 @@
 import streamlit as st
 def photo_library():   
     with st.expander("**First Month**"):
+        st.image(Image.open('one.png'))
         st.markdown("""<a href="https://drive.google.com/drive/folders/1-DMzhVju9hWX-mTmg1psMwJyaSZl54Xd">Click here...</a>""",unsafe_allow_html=True)
     with st.expander("**Second Month**"):
+        st.image(Image.open('two.png'))
         st.markdown("""<a href="https://drive.google.com/drive/folders/1-DMzhVju9hWX-mTmg1psMwJyaSZl54Xd">Click here...</a>""",unsafe_allow_html=True)
     with st.expander("**Third Month**"):
+        st.image(Image.open('three.png'))
         st.markdown("""<a href="https://drive.google.com/drive/folders/1-DMzhVju9hWX-mTmg1psMwJyaSZl54Xd">Click here...</a>""",unsafe_allow_html=True)
     with st.expander("**Fourth Month**"):
+        st.image(Image.open('four.png'))
         st.markdown("""<a href="https://drive.google.com/drive/folders/1-DMzhVju9hWX-mTmg1psMwJyaSZl54Xd">Click here...</a>""",unsafe_allow_html=True)
+    with st.expander("**Fifth Month**"):
+        st.image(Image.open('five.png'))
+        st.markdown("""<a href="https://drive.google.com/drive/folders/1-DMzhVju9hWX-mTmg1psMwJyaSZl54Xd">Click here...</a>""",unsafe_allow_html=True)
+    st.info("Coming Soon. .  .")
