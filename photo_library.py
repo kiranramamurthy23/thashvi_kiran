49aee575c1badf93d5841b8b8989cb6d4a93676f
Kiran Ramamurthy <117091184+kiranramamurthy23@users.noreply.github.com>
2023-04-10 14:37:12 +0530

    Update photo_library.py

--- photo_library.py
+++ photo_library.py
@@ -1,4 +1,10 @@
 import streamlit as st
 def photo_library():   
-    st.success("**First Month**")
-    st.markdown("""<a href="https://drive.google.com/drive/folders/1-DMzhVju9hWX-mTmg1psMwJyaSZl54Xd">Click here...</a>""",unsafe_allow_html=True)
+    with st.expander("**First Month**"):
+        st.markdown("""<a href="https://drive.google.com/drive/folders/1-DMzhVju9hWX-mTmg1psMwJyaSZl54Xd">Click here...</a>""",unsafe_allow_html=True)
+    with st.expander("**Second Month**"):
+        st.markdown("""<a href="https://drive.google.com/drive/folders/1-DMzhVju9hWX-mTmg1psMwJyaSZl54Xd">Click here...</a>""",unsafe_allow_html=True)
+    with st.expander("**Third Month**"):
+        st.markdown("""<a href="https://drive.google.com/drive/folders/1-DMzhVju9hWX-mTmg1psMwJyaSZl54Xd">Click here...</a>""",unsafe_allow_html=True)
+    with st.expander("**Fourth Month**"):
+        st.markdown("""<a href="https://drive.google.com/drive/folders/1-DMzhVju9hWX-mTmg1psMwJyaSZl54Xd">Click here...</a>""",unsafe_allow_html=True)
