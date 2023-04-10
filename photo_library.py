e7c5e621f7952919f47bab0dea4730bb9e42745c
Kiran Ramamurthy <117091184+kiranramamurthy23@users.noreply.github.com>
2023-04-10 10:32:14 +0530

    Update photo_library.py

--- photo_library.py
+++ photo_library.py
@@ -1,4 +1,4 @@
 import streamlit as st
 def photo_library():   
     st.success("**First Month**")
-    st.markdown("""<a href="https://www.google.com/">Click here...</a>""",unsafe_allow_html=True)
+    st.markdown("""<a href="https://drive.google.com/drive/folders/1-DMzhVju9hWX-mTmg1psMwJyaSZl54Xd">Click here...</a>""",unsafe_allow_html=True)
