e92cb20e74110b0b42822b831e125342cea702bb
Kiran Ramamurthy <117091184+kiranramamurthy23@users.noreply.github.com>
2023-04-10 14:54:25 +0530

    Update activity.py

--- activity.py
+++ activity.py
@@ -2,19 +2,25 @@ import streamlit as st
 def activity():
     c1,c2 = st.columns(2, gap="small")
     with c1:
-        st.info("Sleeping")
+        st.info("Sleeping when others are awake 😴")
     with c2:
-        st.success("Drinking Milk")
+        st.info("Drinking milk on demand 🍼")
     
     c1,c2 = st.columns(2, gap="small")
     with c1:
-        st.success("Crying")
+        st.success("Crying for no reasons 😭")
     with c2:
-        st.info("Smiling")
+        st.success("Smile when I'm happy 😃")
         
     c1,c2 = st.columns(2, gap="small")
     with c1:
-        st.info("Crawling")
+        st.info("Crawling to fidget something 🤾‍♀️")
     with c2:
-        st.success("Spitting")
+        st.info("Spitting on everyone 🤤")
+        
+    c1,c2 = st.columns(2, gap="small")
+    with c1:
+        st.success("Seeking attention always 🤸‍♀️")
+    with c2:
+        st.success("Playing with everything except toys 🎮")
     st.warning("**Loading More. . .**")
